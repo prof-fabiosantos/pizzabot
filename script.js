8bd207c4d38bbfa5440f82c15a8a395bbca88699
Prof. Fábio Santos <63064573+prof-fabiosantos@users.noreply.github.com>
2024-10-05 16:15:51 -0400

    Update script.js

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ async function sendMessage() {
     document.getElementById("user-input").value = "";
 
     try {
-        const response = await fetch('http://localhost:8000/chat', {
+        const response = await fetch('https://fabiosantos-backend.hf.space/chat', {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json'
